d8b269c4ed13ecfb54e2d0382cd5820249ecd361
NathanFRANCLET <nathanfranclet5@gmail.com>
2024-11-17 18:37:53 +0100

    q1.1 done

---
+++ exercice1/src/include/Count.hpp
@@ -0,0 +1,18 @@
+template <typename T, T val, T... list>
+class Count;
+
+// Cas général : liste non vide
+template <typename T, T val, T first, T... rest>
+class Count<T, val, first, rest...> {
+public:
+    // Si le premier élément est égal à `val`, continue la récursion
+    enum : int { value = (first == val) ? 1 + Count<T, val, rest...>::value : 0};
+};
+
+// Cas particulier : liste vide
+template <typename T, T val>
+class Count<T, val> {
+public:
+    // Liste vide, retourne 0
+   enum: int { value = 0 };
+};
---
+++ exercice1/src/testCount.cpp
@@ -0,0 +1,26 @@
+#include "Count.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <cstddef>
+
+/**
+ * Programme principal.
+ *
+ * @return @c EXIT_FAILURE car exécution toujours réussie.
+ */
+int
+main() {
+
+    using namespace std;
+    cout << "== [ Count test = begin ] ==" << endl;
+    cout << '\t' << "Count<int, 5 >::value = " << Count<int,5>::value << endl;
+    cout << '\t' << "Count<char, 'a', 'a'>::value = "<< Count<char,'a','a'>::value << endl;
+    cout << '\t' <<"Count<int, 5, 5, 5, 6>:: value = "<< Count<int,5,5,5,6>::value << endl;
+    cout << "== [ Count test = end ] ==" << endl;
+    
+
+
+    // Tout s'est bien passé.
+    return EXIT_SUCCESS;
+
+}
