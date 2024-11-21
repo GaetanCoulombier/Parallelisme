d77433cbb4d278898457f783167316d78a1b412c
Gaëtan-Coulombier <gaetanclb@gmail.com>
2024-11-21 18:09:24 +0100

    Correction du N_FOUND en décrémentant n

--- exercice1/src/include/Found_N_Length_Subset.hpp
+++ exercice1/src/include/Found_N_Length_Subset.hpp
@@ -2,27 +2,34 @@
 #define FOUND_N_LENGTH_SUBSET_HPP
 
 #include <cstddef>
-#include "Count.hpp"
 
 template <typename T, size_t n, T... list>
 class Found_N_Length_Subset;
 
 // Cas général : liste non vide
-template <typename T, size_t n, T val, T... rest>
-class Found_N_Length_Subset<T, n, val, rest...> {
+template <typename T, size_t n, T val, T next, T... rest>
+class Found_N_Length_Subset<T, n, val, next, rest...> {
 public:
-    // On regarde si la sous-liste commence par `val` et contient au moins `n` éléments
     enum : bool {
-        Yes = (Count<T, val, rest...>::value + 1 >= n) ? 1 : Found_N_Length_Subset<T, n, rest...>::Yes
+        Yes = (n == 0) 
+            ? true // Si on cherche une sous-liste de taille 0, elle existe toujours
+            : ((val == next) && Found_N_Length_Subset<T, n - 1, next, rest...>::Yes) 
+              || Found_N_Length_Subset<T, n, next, rest...>::Yes
     };
+};
+
+// Cas particulier : un seul élément
+template <typename T, size_t n, T val>
+class Found_N_Length_Subset<T, n, val> {
+public:
+    enum : bool { Yes = (n == 0) ? true : (n == 1) };
 };
 
 // Cas particulier : liste vide
 template <typename T, size_t n>
 class Found_N_Length_Subset<T, n> {
 public:
-    // Liste vide, il est impossible de trouver une sous-liste d'au moins `n` éléments
-    enum : bool { Yes = (n == 0) ? 1 : 0 };
+    enum : bool { Yes = (n == 0) };
 };
 
 #endif // FOUND_N_LENGTH_SUBSET_HPP
